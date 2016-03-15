ab30385c2dae70fe98c4dd849e355b378a9e6b8d
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-03-15 17:53:59 +0300

    asd

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,11 +1,16 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
-                    :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
-
+    styles: { thumb: "200x200>", medium: "700x500>", big: "1000x1000>" }, 
+    :convert_options => { :medium => '-quality 70 -strip' }, 
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :url  => ":s3_eu_url"
+  
+  Paperclip.interpolates(:s3_eu_url) { |attachment, style|
+  "#{attachment.s3_protocol}://s3-eu-central-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
+  }
   validates_attachment_presence :data
-  validates_attachment_size :data, :less_than => 100.megabytes
-  do_not_validate_attachment_file_type :data
+  validates_attachment_size :data, :less_than => 2.megabytes
+  validates_attachment_content_type :data, :content_type => /\Aimage/
 
   def url_thumb
     @url_thumb ||= Ckeditor::Utils.filethumb(filename)
