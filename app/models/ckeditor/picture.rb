035258589dd89d1319145e4ad87d7f330a50606a
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-03-15 17:50:43 +0300

    sdf

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,9 +1,13 @@
 class Ckeditor::Picture < Ckeditor::Asset
-  has_attached_file :data,
-                    :url  => "/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :path => ":rails_root/public/ckeditor_assets/pictures/:id/:style_:basename.:extension",
-                    :styles => { :content => '800>', :thumb => '118x100#' }
-
+ has_attached_file :data,
+    styles: { thumb: "200x200>", medium: "700x500>", big: "1000x1000>" }, 
+    :convert_options => { :medium => '-quality 70 -strip' }, 
+    :path => '/:class/:attachment/:id_partition/:style/:filename',
+    :url  => ":s3_eu_url"
+  
+  Paperclip.interpolates(:s3_eu_url) { |attachment, style|
+  "#{attachment.s3_protocol}://s3-eu-central-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, "")}"
+  }
   validates_attachment_presence :data
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_content_type :data, :content_type => /\Aimage/
