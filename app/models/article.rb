96fbc1dab130bd4ecb710fe4b1221e00f30ccab4
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-03-18 12:25:35 +0300

    fixed

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,6 @@ class Article < ActiveRecord::Base
  belongs_to :image
   include Galleryable
   has_one :gallery, as: :galleryable, dependent: :destroy
-  has_attached_file :image,
-    styles: { thumb: "200x200>", catalog: '350x250>', medium: "500x500>", big: "1000x1000>" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   after_save :set_preview
 
   def set_preview
