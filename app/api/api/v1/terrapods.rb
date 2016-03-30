0701ba50a0dd5b25b787488c884a6f4ba058669f
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-03-30 19:41:14 +0300

    api fixed for preview

--- app/api/api/v1/terrapods.rb
+++ app/api/api/v1/terrapods.rb
@@ -3,7 +3,9 @@ module API
     class TerrapodPreview < Grape::Entity
       expose :id, documentation: { type: "Integer", desc: "id"}
       expose :title, documentation: { type: "String", desc: "Заоловок"}
-      expose :preview, documentation: { type: "Attachment", desc: "Одна картинка"}
+      expose :preview, documentation: { type: "Attachment", desc: "Одна картинка"} do |terra|
+				terra.preview.url if terra.image
+			end
       expose :video, documentation: { type: "String", desc: "Код"} do |video|
 				video.video.split('?')[1][/v=[a-zA-Z\d]+/][2..-1] if (video.video && !(video.video.empty?))
 			end
