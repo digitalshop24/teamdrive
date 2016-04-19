81ff7ff6a741e48cc3cbdf5131d8e52d32f2b902
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-04-19 16:01:50 +0300

    df

--- app/api/api/v1/articles.rb
+++ app/api/api/v1/articles.rb
@@ -1,11 +1,12 @@
 module API
   module Entities
     class ArticlePreview < Grape::Entity
+      regex = /^(?:http(?:s)?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/
       expose :id, documentation: { type: "Integer", desc: "id"}
       expose :title, documentation: { type: "String", desc: "Заоловок"}
       expose :body, documentation: { type: "String", desc: "Статья"}
       expose :video, documentation: { type: "String", desc: "Код"} do |video|
-				video.video.split('?')[1][/v=[a-zA-Z\d]+/][2..-1] if (video.video && !(video.video.empty?))
+				video.video.scan(regex).flatten.first if (video.video && !(video.video.empty?))
 			end
       expose :description, documentation: { type: "String", desc: "Описание"}
       expose :image, documentation: { type: "Array", desc: "Kартинки"} do |event|
